dafe25bc79147748f675b5f9f98f56106dc0be5b
Vishal <125354693+rs-vishal@users.noreply.github.com>
2025-03-01 20:28:21 +0530

    Time: 20 ms (4.8%), Space: 43.7 MB (56.58%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -3,13 +3,11 @@ class Solution {
         int n = nums.length;
         for(int i=0;i<n-1;i++){
             if(nums[i] == nums[i+1]){
-                nums[i] = nums[i] +nums[i+1];
+                nums[i] = nums[i] *2;
                 nums[i+1]=0;
             }
         }
-        // for(int i=0;i<n;i++){
-        //     System.out.println(nums[i]);
-        // }
+        
         for (int j = 0; j < n - 1; j++) {  
     for (int i = 0; i < n - 1; i++) {
         if (nums[i] == 0) {
