8b682c8102454a2724f1e2400fd9a3addd915b19
Vishal <125354693+rs-vishal@users.noreply.github.com>
2025-03-31 22:12:01 +0530

    Time: 9 ms (96.06%), Space: 43.2 MB (64.17%) - LeetHub

--- 0869-similar-string-groups/0869-similar-string-groups.java
+++ 0869-similar-string-groups/0869-similar-string-groups.java
@@ -1,30 +1,39 @@
 class Solution {
-    public int numSimilarGroups(String[] strs) {
-        int groups = 0, n = strs.length;
-        boolean[] vis = new boolean[n];
-        for(int i = 0; i < strs.length; i++){
-            if(vis[i]) continue;
-            groups++;
-            dfs(i, strs, vis);
+    private static boolean areSimilar(String s1, String s2) {
+        int count = 0;
+        if (s1.equals(s2)) return true;
+
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                count++;
+                if (count > 2) return false;
+            }
         }
-        return groups;
+        return count == 2;
+    }
+     public static int numSimilarGroups(String[] strs) {
+        int n = strs.length;
+        boolean[] visited = new boolean[n];
+        int groupCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                groupCount++;
+                findGroup(strs, visited, i);
+            }
+        }
+        
+        return groupCount;
     }
 
-    void dfs(int i, String[] strs, boolean[] vis){
-        vis[i] = true;
-        for(int j = 0; j < strs.length; j++){
-            if(vis[j]) continue;
-            if(isSimilar(strs[i], strs[j])){
-                dfs(j, strs, vis);
+    private static void findGroup(String[] strs, boolean[] visited, int index) {
+        visited[index] = true;
+
+        for (int i = 0; i < strs.length; i++) {
+            if (!visited[i] && areSimilar(strs[index], strs[i])) {
+                findGroup(strs, visited, i);
             }
         }
     }
 
-    boolean isSimilar(String a, String b){
-        int count = 0;
-        for(int i = 0; i < a.length(); i++){
-            if(a.charAt(i) != b.charAt(i)) count++;
-        }
-        return (count == 2 || count == 0);
-    }
 }
