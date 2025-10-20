7fd090529c775bb302c82b3d905ffc464be8f9e3
Vishal <125354693+rs-vishal@users.noreply.github.com>
2025-10-20 16:55:12 +0530

    Time: 23 ms (37.08%), Space: 45.4 MB (24.12%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,22 +1,20 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int len = s.length();
-        StringBuilder  res = new StringBuilder();
-        for(int i=0;i<len;i++){
-            int l = i;
-            int r= i;
-            while( l>=0 && r<len && s.charAt(l) == s.charAt(r)){
-                if(r - l +1  > res.length()){
-                res= new StringBuilder(s.substring(l,r+1));
+        StringBuilder res = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            int l=i,r=i;
+            while(l>=0 && r <s.length() && s.charAt(l) == s.charAt(r)){
+                if(r-l+1 > res.length()){
+                    res = new StringBuilder(s.substring(l,r+1));
                 }
                 l-=1;
                 r+=1;
             }
-            l=i;
-            r=i+1;
-            while( l>=0 && r<len && s.charAt(l) == s.charAt(r)){
-                if(r - l +1  > res.length()){
-                res= new StringBuilder(s.substring(l,r+1));
+
+            l=i;r=i+1;
+            while(l>=0 && r <s.length() && s.charAt(l) == s.charAt(r)){
+                if(r-l+1 > res.length()){
+                    res = new StringBuilder(s.substring(l,r+1));
                 }
                 l-=1;
                 r+=1;
